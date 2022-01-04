690b98102d4462a05f8c3076756d3bd86371449e
Charles Willis <5862883+trippwill@users.noreply.github.com>
2022-01-04 17:11:18 +0000

    Trim, disable lloc

--- cloudflare-pages-publish.sh
+++ cloudflare-pages-publish.sh
@@ -1,9 +1,3 @@
-# Get the emsdk repo
-git clone https://github.com/emscripten-core/emsdk.git
-./emsdk/emsdk install latest
-./emsdk/emsdk activate latest
-source ./emsdk/emsdk_env.sh
-
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh;
 chmod +x dotnet-install.sh;
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6;
